3054833fb8b1380a7603b3d78cdc7c7ee051b82a
unknown <oparia@ISOPARIAL1C.corp.emc.com>
2013-04-07 10:22:32 +0300

    Changing version + update to generator infrastructure

--- jbehave-stress/src/main/java/com/stresstest/jbehave/context/StoryContext.java
+++ jbehave-stress/src/main/java/com/stresstest/jbehave/context/StoryContext.java
@@ -5,78 +5,88 @@ import java.util.Map;
 
 public class StoryContext {
 
-    final private ThreadLocal<Map<String, Map<Class<?>, Object>>> implementation = new ThreadLocal<Map<String, Map<Class<?>, Object>>>() {
-        @Override
-        public Map<String, Map<Class<?>, Object>> initialValue() {
-            return new HashMap<String, Map<Class<?>, Object>>();
-        }
-    };
+	final private ThreadLocal<Map<String, Map<Class<?>, Object>>> implementation = new ThreadLocal<Map<String, Map<Class<?>, Object>>>() {
+		@Override
+		public Map<String, Map<Class<?>, Object>> initialValue() {
+			return new HashMap<String, Map<Class<?>, Object>>();
+		}
+	};
 
-    public StoryContext() {
-    }
+	final private ThreadLocal<String> lastActor = new ThreadLocal<String>();
 
-    public void put(Object key, Object value) {
-        if (value == null || key == null || value.getClass() == Void.class)
-            return;
-        String keyString = StoryContextReflection.getName(key);
+	public StoryContext() {
+	}
 
-        Map<String, Map<Class<?>, Object>> objectMap = implementation.get();
-        if (objectMap.get(keyString) == null)
-            objectMap.put(keyString, new HashMap<Class<?>, Object>());
-        objectMap.get(keyString).put(normalize(value.getClass()), value);
-    }
+	public void put(Object key, Object value) {
+		if (value == null || key == null || value.getClass() == Void.class)
+			return;
+		String keyString = StoryContextReflection.getName(key);
 
-    public Object get(String name, Class<?> targetClass) {
-        Object value = internalGet(name, targetClass);
-        if (value != null)
-            value = StoryContextReflection.setName(value, name);
-        return value;
-    }
+		Map<String, Map<Class<?>, Object>> objectMap = implementation.get();
+		if (objectMap.get(keyString) == null)
+			objectMap.put(keyString, new HashMap<Class<?>, Object>());
+		objectMap.get(keyString).put(normalize(value.getClass()), value);
 
-    public Map<Class<?>, Object> get(String name) {
-        return implementation.get().get(name);
-    }
+		lastActor.set(keyString);
+	}
+	
+	public Object get(Class<?> targetClass) {
+		return get(lastActor.get(), targetClass);
+	}
 
-    private Object internalGet(String name, Class<?> targetClass) {
-        targetClass = normalize(targetClass);
+	public Object get(String name, Class<?> targetClass) {
+		Object value = internalGet(name, targetClass);
+		if (value != null) {
+			value = StoryContextReflection.setName(value, name);
+			lastActor.set(name);
+		}
+		return value;
+	}
 
-        Map<Class<?>, Object> valueMap = implementation.get().get(name);
-        if (valueMap == null)
-            return null;
-        if (valueMap.containsKey(targetClass)) {
-            return valueMap.get(targetClass);
-        }
+	public Map<Class<?>, Object> get(String name) {
+		return implementation.get().get(name);
+	}
 
-        for (Class<?> keyClass : valueMap.keySet()) {
-            if (targetClass.isAssignableFrom(keyClass))
-                return valueMap.get(keyClass);
-        }
+	private Object internalGet(String name, Class<?> targetClass) {
+		targetClass = normalize(targetClass);
 
-        return null;
-    }
+		Map<Class<?>, Object> valueMap = implementation.get().get(name);
+		if (valueMap == null)
+			return null;
+		if (valueMap.containsKey(targetClass)) {
+			return valueMap.get(targetClass);
+		}
 
-    public void clear() {
-        implementation.get().clear();
-    }
+		for (Class<?> keyClass : valueMap.keySet()) {
+			if (targetClass.isAssignableFrom(keyClass))
+				return valueMap.get(keyClass);
+		}
 
-    private Class<?> normalize(Class<?> targetClass) {
-        if (targetClass == byte.class)
-            return Byte.class;
-        if (targetClass == char.class)
-            return Character.class;
-        if (targetClass == short.class)
-            return Short.class;
-        if (targetClass == int.class)
-            return Integer.class;
-        if (targetClass == long.class)
-            return Long.class;
-        if (targetClass == double.class)
-            return Double.class;
-        if (targetClass == float.class)
-            return Float.class;
-        if (targetClass == boolean.class)
-            return Boolean.class;
-        return targetClass;
-    }
+		return null;
+	}
+
+	public void clear() {
+		implementation.get().clear();
+	}
+
+	private Class<?> normalize(Class<?> targetClass) {
+		if (targetClass == byte.class)
+			return Byte.class;
+		if (targetClass == char.class)
+			return Character.class;
+		if (targetClass == short.class)
+			return Short.class;
+		if (targetClass == int.class)
+			return Integer.class;
+		if (targetClass == long.class)
+			return Long.class;
+		if (targetClass == double.class)
+			return Double.class;
+		if (targetClass == float.class)
+			return Float.class;
+		if (targetClass == boolean.class)
+			return Boolean.class;
+		return targetClass;
+	}
 
 }
